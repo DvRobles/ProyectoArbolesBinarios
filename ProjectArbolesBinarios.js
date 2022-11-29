e0044c04e29e920532c400edd979583e582e57e0
David Santana <89950671+DvRobles@users.noreply.github.com>
2022-11-28 21:58:26 -0600

    falta resultado final pre y pos

--- ProjectArbolesBinarios.js
+++ ProjectArbolesBinarios.js
@@ -1,70 +1,4 @@
 
-// class Node{
-//     constructor(value){
-//         this.value = value;
-//         this.lhs = null;   //izquierdo
-//         this.rhs = null;   //derecho
-//         this.next = null;
-//         this.prev = null;
-//     }
-// }
-
-// class binaryTree{
-//     constructor(){
-//         this.root = null;
-//     }
-
-//     // add(node){
-//     //     if(this.root==null){
-//     //         this.root=node;
-//     //     }else {
-//     //         let aux = this.root;
-//     //         while(aux.next!=null){
-//     //             aux = aux.next;
-//     //         }
-//     //         aux.next=node;
-//     //         nodo.prev=aux;
-//     //     }
-//     // }
- 
-
-//     makeTree(){
-        
-//     }
-
-
-
-//     postO(node, array){
-//         if(!node) return;
-//         this.postO(node.lhs, array);
-//         this.postO(node.rhs, array);
-//         array.push(node.value);
-//         return array;
-//     }
-
-
-
-//     preO(node, array){
-//         if(!node) return;
-//         array.push(node.value);
-//         this.preO(node.lhs, array);
-//         this.preO(node.rhs, array);
-//         return array;
-//     }
-
-
-
-
-    
-
-// }
-
-
-
-
-
-
-
 class Node {
     constructor(value){
         this.value = value;
@@ -73,115 +7,86 @@ class Node {
         this.next = null;
         this.prev = null;
     }
-
-
-    add(value) {
-        if (value < this.value) {
-            this.addToTheLeft(value);
-        } else {
-            this.addToTheRight(value);
-        }
-    }
-
-    addToTheLeft(value) {
-        if (this.lhs) {
-            this.lhs.add(value);
-        } else {
-            this.lhs = new Node(value);
-        }
-    }
-
-    addToTheRight(value) {
-        if (this.rhs) {
-            this.rhs.add(value);
-        } else {
-            this.rhs = new Node(value);
-        }
-    }
 }
 
-
-// Alumnos: 
-// GARCIA GONZALEZ ANDREA '3-I'.
-// SANTANA ROBLES DAVID ALEJANDRO '3-I'.
-
-class Nodo{
-    constructor(numero){
-        this.numero=numero;
-        this.sig=null;
-        this.ant=null;
-    }
-}
 class ListaDoble{
     constructor(){
-        this.primero=null;
+        this.first=null;
         this.ultimo=null;
         this.root=null;
     }
 
     agregar(nuevo){
-        if (this.primero==null){
-            this.primero=nuevo;
+        if (this.first==null){
+            this.first=nuevo;
             this.ultimo=nuevo;
         }else{
-            this.ultimo.sig=nuevo
-            nuevo.ant=this.ultimo;
+            this.ultimo.next=nuevo
+            nuevo.prev=this.ultimo;
             this.ultimo=nuevo
         }
     }
-    listar(){
+
+    listar(){ //solo lo uso para checar
         let res="";
-        let temp=this.primero;
+        let temp=this.first;
         while(temp!=null){
-            res+=temp.numero + "  ";
-            temp=temp.sig;
+            res+=temp.value + "  ";
+            temp=temp.next;
         }
         return res;
     }
-    invertir(){
-        let aux = this.primero;
-        let temp = null;
-        while(aux != null){
-            temp = aux.ant;
-            aux.ant = aux.sig;
-            aux.sig = temp;
-            aux = aux.ant;
+
+    makeTree() {
+        let aux = this.first;
+        let xpression;
+        while (aux) {
+            xpression = aux.value === '/' || aux.value === '*';
+            if(xpression) {
+                aux.lhs = aux.prev;
+                aux.rhs = aux.next;
+                this.drop(aux.prev);
+                this.drop(aux.next);
+            }
+            aux = aux.next;
         }
-        this.primero = this.ultimo;
-        this.ultimo = this.primero;
+
+        aux = this.first
+        while (aux) {
+            xpression = aux.value === '+' || aux.value === '-';
+            if(xpression) {
+                aux.lhs = aux.prev;
+                aux.rhs = aux.next;
+                this.drop(aux.prev);
+                this.drop(aux.next);
+            }
+            if (!aux.next) {
+                this.root = aux;
+            }
+            aux = aux.next
+        }
+        return this.root;
+    }
+
+    drop(elemento){
+        if(this.first == elemento) {
+            this.first = this.first.next;
+            return;
+        }
+        if(elemento.next && elemento.prev) {
+            elemento.next.prev = elemento.prev;
+            elemento.prev.next = elemento.next;
         return;
+        }
+
+        if(!elemento.next) {
+        this.ultimo = elemento.prev;
+        elemento.prev.next = elemento.next;
+        return;
+        }
     }
 
-    crearArbol() {
-        condition = element === '/' || element === '*'
-        check(condition)
-
-        condition = element === '+' || element === '-'
-        check(condition)
-
-        
-    }
-}
-
-let ldoble=new ListaDoble();
-let nodo=new Nodo(2);
-ldoble.agregar(nodo);
-nodo=new Nodo('+');
-ldoble.agregar(nodo);
-nodo=new Nodo(7);
-ldoble.agregar(nodo);
-nodo=new Nodo('*');
-ldoble.agregar(nodo);
-nodo=new Nodo(9);
-ldoble.agregar(nodo);
-ldoble.listar();
-console.log(ldoble.listar());
-ldoble.invertir();
-console.log(ldoble.listar());
-class binaryTree{
-    constructor() {
-
-    }
+    //preOrder es RID
     preO(Node, array){
         if(!Node) return;
         array.push(Node.value);
@@ -190,29 +95,64 @@ class binaryTree{
         return array;
     }
 
+    //preOrder es IDR
     postO(Node, array){
-        if(!Node) return;
+        if(!Node) return;   
         this.postO(Node.lhs, array);
         this.postO(Node.rhs, array);
         array.push(Node.value);
         return array;
     }
 
+    preOrResultado(){
+        
+
+    }
+
+    postOrResultado(){
+
+    }
 }
 
 
+// expresion A EVALUAR = 2  +  7  *  9  /  7 ;
+//result pre [ 2, 7, 9, '*', 7, '/', '+' ]
+//result pos [ '+', 2, '/', '*', 7, 9, 7 ]
+// resultado debe dar 11 en pre y pos; 
+//pd. si se lograrón los resultados;
 
 
-const head = new Node(15);
-const binary = new binaryTree();
-head.add(10);
-head.add(90);
-head.add(8);
-head.add(40);
-head.add(20);
-head.add(16);
 
-//console.log(head);
+let ldoble = new ListaDoble();
+let arrayPre = [];
+let arrayPos = [];
 
-console.log(binary.postO(head, []))
-console.log(binary.preO(head, []))
+let nodo=new Node(2);
+ldoble.agregar(nodo);
+nodo=new Node('+');
+ldoble.agregar(nodo);
+nodo=new Node(7);
+ldoble.agregar(nodo);
+nodo=new Node('*');
+ldoble.agregar(nodo);
+nodo=new Node(9);
+ldoble.agregar(nodo);
+nodo=new Node("/");
+ldoble.agregar(nodo);
+nodo=new Node(7);
+ldoble.agregar(nodo);
+
+
+console.log(ldoble.listar());
+console.log(ldoble.makeTree());
+console.log(ldoble.postO(ldoble.first, arrayPos))
+console.log(ldoble.preO(ldoble.first, arrayPre))
+
+
+
+
+
+
+
+
+
